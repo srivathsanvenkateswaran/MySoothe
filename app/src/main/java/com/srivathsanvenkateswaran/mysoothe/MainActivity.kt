964bb8d663f12e2d3ef6646c2e8971314ce30851
srivathsanvenkateswaran <srivathsanvenkateswaran@gmail.com>
2021-07-18 22:14:48 +0530

    added WelcomeScreen.kt

--- app/src/main/java/com/srivathsanvenkateswaran/mysoothe/MainActivity.kt
+++ app/src/main/java/com/srivathsanvenkateswaran/mysoothe/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.srivathsanvenkateswaran.mysoothe.composables.WelcomeScreen
 import com.srivathsanvenkateswaran.mysoothe.ui.theme.MySootheTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +16,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MySootheTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                WelcomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MySootheTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/srivathsanvenkateswaran/mysoothe/composables/WelcomeScreen.kt
@@ -0,0 +1,137 @@
+package com.srivathsanvenkateswaran.mysoothe.composables
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.srivathsanvenkateswaran.mysoothe.R
+import com.srivathsanvenkateswaran.mysoothe.ui.theme.MySootheTheme
+
+@Composable
+fun WelcomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = MaterialTheme.colors.background
+            )
+    ) {
+        WelcomeBackground()
+
+        LogoButtonColumn(
+            modifier = Modifier
+                .align(Alignment.Center)
+        )
+    }
+}
+
+@Composable
+private fun LogoButtonColumn(
+    modifier: Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .padding(
+                horizontal = 16.dp
+            )
+    ) {
+        MySootheLogo()
+        
+        Spacer(modifier = Modifier.height(32.dp))
+
+        MySootheButton(
+            onClick = {},
+            buttonText = "SIGN UP",
+            buttonColor = MaterialTheme.colors.primary
+        )
+        
+        Spacer(modifier = Modifier.height(8.dp))
+
+        MySootheButton(
+            onClick = {},
+            buttonText = "LOG IN",
+            buttonColor = MaterialTheme.colors.secondary
+        )
+    }
+}
+
+@Composable
+private fun MySootheLogo() {
+
+    val isLight = MaterialTheme.colors.isLight
+
+    val appLogoRes = if(isLight) {
+        R.drawable.ic_light_logo
+    } else {
+        R.drawable.ic_dark_logo
+    }
+
+    Image(
+        painter = painterResource(id = appLogoRes),
+        contentDescription = "MySoothe Logo"
+    )
+}
+
+@Composable
+private fun MySootheButton(
+    onClick: () -> Unit,
+    buttonText: String,
+    buttonColor: Color
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .height(72.dp)
+            .fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = buttonColor
+        )
+    ) {
+        Text(text = buttonText)
+    }
+}
+
+@Composable
+private fun WelcomeBackground() {
+    val isLight = MaterialTheme.colors.isLight
+
+    val backgroundResource = if (isLight) {
+        R.drawable.ic_light_welcome
+    } else {
+        R.drawable.ic_dark_welcome
+    }
+
+    Image(
+        painter = painterResource(id = backgroundResource),
+        contentDescription = null,
+        modifier = Modifier
+            .fillMaxSize(),
+        contentScale = ContentScale.FillBounds
+    )
+}
+
+@Preview(
+    name = "Night Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Preview(
+    name = "Day Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Composable
+private fun WelcomeScreenPreview() {
+    MySootheTheme {
+        WelcomeScreen()
+    }
+}
